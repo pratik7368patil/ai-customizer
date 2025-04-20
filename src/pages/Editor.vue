cd19984b80d4f23a0a3bf9f7550f78d6a5ce455b
Pratik Patil <pratik7368patil@gmail.com>
2025-04-20 16:58:08 +0530

    Initial commit: Vue project setup with default grapesjs SDK

---
+++ src/pages/Editor.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import createStudioEditor from '@grapesjs/studio-sdk';
+import '@grapesjs/studio-sdk/style';
+
+// ...
+createStudioEditor({
+  licenseKey: 'your-license-key',
+  project: {
+    type: 'web',
+    // The default project to use for new projects
+    default: {
+      pages: [
+        { name: 'Home', component: '<h1>Home page</h1>' },
+        { name: 'About', component: '<h1>About page</h1>' },
+        { name: 'Contact', component: '<h1>Contact page</h1>' },
+      ]
+    },
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Editor</h1>
+  </div>
+</template>
+
+
